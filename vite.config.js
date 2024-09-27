9ecb5cc28ae216fd5791c40f4f99842bfa7baa1d
Jakob Eschler <mail@jakob-eschler.de>
2024-09-27 13:50:08 +0200

    config changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+			"~": fileURLToPath(new URL("./node_modules", import.meta.url)),
+		},
+	},
+  css: {
+		preprocessorOptions: {
+			scss: {
+				// global scss data
+				additionalData: `
+          @import "~/uikit/src/scss/variables-theme.scss";
+          @import "~/uikit/src/scss/mixins-theme.scss";
+          @import "~/uikit/src/scss/uikit.scss";
+        `,
+			},
+		},
+	}
 })
